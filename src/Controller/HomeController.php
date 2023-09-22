5c5cbb30348dc558baa828285b9711191d9b2864
johnBeGoode <jonathan.martin084@gmail.com>
2023-09-22 14:34:57 +0200

    Form contact + design

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -21,9 +24,18 @@ class HomeController extends AbstractController
     }
 
     #[Route('/contact-us', name: 'app_contact')]
-    public function contact(): Response
-    {
-        return $this->render('home/contact.html.twig', []);
+    public function contact(Request $request): Response
+    {   
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('Success', 'Votre message a bien été envoyé !' );
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('home/contact.html.twig', [
+            'form' => $form
+        ]);
     }
 
     #[Route('/about', name: 'app_about')]
